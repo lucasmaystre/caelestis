7113e1824b1243cf6ef897e6ddc8c8efe22b05b0
Lucas Maystre <lucas@maystre.ch>
2020-12-24 11:03:02 +0100

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
 
 setuptools.setup(
     name="celestine",
-    version="0.2.0",
+    version="0.2.1",
     author="Lucas Maystre",
     author_email="lucas@maystre.ch",
     description="Knowledge management system.",
